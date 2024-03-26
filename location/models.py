c600a5fb60b50815808c1e206c5f4aa070c6f91f
iriskin77 <talcore@yandex.ru>
2024-03-26 14:56:31 +0500

    add: location model

---
+++ location/models.py
@@ -0,0 +1,14 @@
+from tortoise import models
+from tortoise import fields
+
+
+class Location(models.Model):
+
+    id = fields.IntField(pk=True, unique=True)
+    city = fields.CharField(max_length=255)
+    state = fields.CharField(max_length=255)
+    zip = fields.IntField()
+    latitude = fields.FloatField()
+    longitude = fields.FloatField()
+
+
